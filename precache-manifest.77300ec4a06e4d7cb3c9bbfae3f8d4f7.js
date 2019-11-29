ce5c1b92d814e06f55788925cf0822245bae6f36
CircleCI <builds@circleci.com>
2019-11-29 22:09:45 +0000

    Updates

---
+++ precache-manifest.77300ec4a06e4d7cb3c9bbfae3f8d4f7.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ec87e4ad1b16f0c457eaadc8c8bb2dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af6cdb642319bdbf8b09",
+    "url": "/static/css/2.22921dbe.chunk.css"
+  },
+  {
+    "revision": "d85886cba798d03c53d8",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "af6cdb642319bdbf8b09",
+    "url": "/static/js/2.5fc1a879.chunk.js"
+  },
+  {
+    "revision": "d85886cba798d03c53d8",
+    "url": "/static/js/main.7eede673.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "5164d463ce4b4aceb65b2929fb1b0b0d",
+    "url": "/static/media/button-click.5164d463.wav"
+  },
+  {
+    "revision": "e2c0505c6c37b052d43a3875b596c80f",
+    "url": "/static/media/data-security-icon.e2c0505c.svg"
+  },
+  {
+    "revision": "0fca856e4b15a029c9973565c9aa9469",
+    "url": "/static/media/database-icon.0fca856e.svg"
+  },
+  {
+    "revision": "6857f822c6fca023b4eda19c1181cd07",
+    "url": "/static/media/developer-icon.6857f822.svg"
+  },
+  {
+    "revision": "ec4b277306658f25e01862fcd857714a",
+    "url": "/static/media/dp1.ec4b2773.jpg"
+  },
+  {
+    "revision": "bedf3c0c0d8876c8198de3f9bd9b1298",
+    "url": "/static/media/optimization-icon.bedf3c0c.svg"
+  },
+  {
+    "revision": "4a8ba14e61945d844195d519968b0cc6",
+    "url": "/static/media/sellvy.4a8ba14e.png"
+  },
+  {
+    "revision": "dedb4e63ef18f28d6c24957e6224f513",
+    "url": "/static/media/sellvy2.dedb4e63.png"
+  },
+  {
+    "revision": "4f360d254abf63fce5929c7ad99e206f",
+    "url": "/static/media/sellvy3.4f360d25.png"
+  },
+  {
+    "revision": "dea9003210a25a427c224214b63e1258",
+    "url": "/static/media/sellvy4.dea90032.png"
+  },
+  {
+    "revision": "18c9e439436b8285ec02ce5e564b869e",
+    "url": "/static/media/sellvy5.18c9e439.png"
+  },
+  {
+    "revision": "e19817f0a604e3dead781e1a3a6421f3",
+    "url": "/static/media/shuttlers.e19817f0.png"
+  },
+  {
+    "revision": "3a1b561ed58f9447247ee586c479d54f",
+    "url": "/static/media/shuttlers1.3a1b561e.png"
+  },
+  {
+    "revision": "febd745220fb677916c1c43045af8b4b",
+    "url": "/static/media/tantasecure.febd7452.png"
+  },
+  {
+    "revision": "60e468c21730ffeab3a923410e110df0",
+    "url": "/static/media/tantasecure1.60e468c2.png"
+  },
+  {
+    "revision": "8b22ff6b7f5e1f8b1b863c822e183746",
+    "url": "/static/media/tantasecure2.8b22ff6b.png"
+  },
+  {
+    "revision": "929820a64808ff5e9bb308ebb623f272",
+    "url": "/static/media/tantasecure3.929820a6.png"
+  },
+  {
+    "revision": "e8c91179cb714ebe3951a238033538fe",
+    "url": "/static/media/tantasecure4.e8c91179.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.600d4c8d0e785ec37c27f4e7f28c763f.js"
+  "/precache-manifest.77300ec4a06e4d7cb3c9bbfae3f8d4f7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
